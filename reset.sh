1d67c29e17773978b97a91e7593e8b934984f95d
Patrick Riel <priel@cisco.com>
2020-04-28 13:10:37 -0700

    adding config for mvsense parser

--- reset.sh
+++ reset.sh
@@ -2,4 +2,8 @@ docker-compose down
 rm -r kapacitor/data
 rm -r influxdb/data
 rm -r chronograf/data
+rm -r images/telegraf/bin
+mkdir -p images/telegraf/bin
+cp $GOPATH/src/github.com/influxdata/telegraf/telegraf images/telegraf/bin/telegraf
+docker-compose build telegraf
 docker-compose up -d
